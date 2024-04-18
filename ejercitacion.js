add6134bb8cf379c7eb1613d05ecdb2cc70b3990
Agustin <riosagustin843@gmail.com>
2024-04-18 14:52:28 -0300

    actualizacion de ejercitacion-a

--- ejercitacion.js
+++ ejercitacion.js
@@ -1,4 +1,4 @@
-//Realizar un programa que genere un numero aletorio del 1 al 1000 y que la maquina lo adivine
+/* //Realizar un programa que genere un numero aletorio del 1 al 1000 y que la maquina lo adivine
 let randomNum = 0;
 let min = 0;
 let max = 0;
@@ -39,4 +39,112 @@ if(prediction != -1){
 }
 else{
     console.log("La maquina no aceerto el numero, el numero era: "+randomNum)
+} */
+/* Ejemplo en ingles 
+//Ahora al programa para calcular las notas lo vamos a refactorizar utilizando Objetos.
+//creo un objeto llamado machine que va a ser la maquina que adivine el numero
+let machine = {
+    randomNum: 0,
+    max: 0,
+    min: 0,
+    counter: 0,
+    //inicio dando valores que voy a ocupar para poder adivinar el numero
+    init: function() {
+        this.min = 1;
+        this.max = 1000;
+        this.counter = 0;
+        this.randomNum = this.randomNumFunction(this.min, this.max);
+        console.log(this.randomNum);
+    },
+    //genero un método para generar un numero random
+    randomNumFunction: function (){
+        return Math.floor(Math.random() * (this.max*this.min)) - this.min
+    },
+    //genero un método para poder buscar el numero random generado
+    binarySearch: function() {
+        let left = this.min;
+        let right = this.max;
+        while (left <= right) {
+            this.counter++;
+            let prediction = Math.floor((left + right) / 2);
+            if (prediction === this.randomNum) {
+                this.prediction = prediction;
+                return;
+            } else if (prediction < this.randomNum) {
+                left = prediction + 1;
+            } else {
+                right = prediction - 1;
+            }
+        }
+        this.prediction = -1;
+    },
+    //metodo para imprimir si la prediccion fue acertada o no
+    result: function(){
+        if (prediction !== -1) {
+            console.log("La máquina acertó el número: " + this.prediction + ". Se tardó un total de: " + this.counter + " intentos");
+        } else {
+            console.log("La máquina no acertó el número. El número era: " + this.randomNum);
+        }
+    }
 }
+//llamo a machine y sus metodos para iniciar
+machine.init();
+let prediction = machine.binarySearch();
+let result = machine.result(prediction);
+*/
+/* //Ejemplo en español
+//Ahora al programa para calcular las notas lo vamos a refactorizar utilizando Objetos.
+//creo un objeto llamado maquina que va a ser la maquina que adivine el numero
+let maquina = {
+    numeroAleatorio: 0,
+    maximo: 0,
+    minimo: 0,
+    contador: 0,
+
+    // Inicializa los valores necesarios para el juego
+    iniciar: function() {
+        this.minimo = 1;
+        this.maximo = 1000;
+        this.contador = 0;
+        this.numeroAleatorio = this.generarNumeroAleatorio();
+        console.log(this.numeroAleatorio);
+    },
+
+    // Genera un número aleatorio
+    generarNumeroAleatorio: function() {
+        return Math.floor(Math.random() * (this.maximo * this.minimo)) - this.minimo;
+    },
+
+    // Realiza la búsqueda binaria para adivinar el número aleatorio
+    busquedaBinaria: function() {
+        let izquierda = this.minimo;
+        let derecha = this.maximo;
+        while (izquierda <= derecha) {
+            this.contador++;
+            let prediccion = Math.floor((izquierda + derecha) / 2);
+            if (prediccion === this.numeroAleatorio) {
+                this.prediccion = prediccion;
+                return;
+            } else if (prediccion < this.numeroAleatorio) {
+                izquierda = prediccion + 1;
+            } else {
+                derecha = prediccion - 1;
+            }
+        }
+        this.prediccion = -1;
+    },
+
+    // Imprime el resultado del juego
+    resultado: function() {
+        if (this.prediccion !== -1) {
+            console.log("La máquina adivinó el número: " + this.prediccion + ". Se tardó un total de: " + this.contador + " intentos");
+        } else {
+            console.log("La máquina no adivinó el número. El número era: " + this.numeroAleatorio);
+        }
+    }
+}
+
+// Inicia el juego
+maquina.iniciar();
+let prediccion = maquina.busquedaBinaria();
+let resultado = maquina.resultado(prediccion); */
